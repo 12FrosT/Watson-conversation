23fc017b368ea183487ae25ef066c045f90021b5
12FrosT <ashishchauhan1206@gmail.com>
2018-02-04 00:13:53 +0530

    added cookie based sessions

--- microservices/chat/app/src/server.js
+++ microservices/chat/app/src/server.js
@@ -16,39 +16,51 @@ var conversation = new watson.ConversationV1({
 app.use(cookieParser());
 app.use(bodyParser.json());
 
-var context=null;
+var context=[];
 
 app.get('/', function (req, res) {
-  conversation.message({
-      input: {'text': req.body.text},
-      workspace_id: '1cf9cd65-5506-4bc2-a31d-06872dec90dd',
-      context: context
-  },  function(err, response) {
-      if (err)
-      console.log('error:', err);
-      else
-     //console.log(JSON.stringify(response, null, 2));
-     context= response.context;
+  // conversation.message({
+  //     input: {'text': req.body.text},
+  //     workspace_id: '1cf9cd65-5506-4bc2-a31d-06872dec90dd',
+  //     context: context
+  // },  function(err, response) {
+  //     if (err)
+  //     console.log('error:', err);
+  //     else
+  //    //console.log(JSON.stringify(response, null, 2));
+  //    contexts= response.context;
      res.sendfile(path.join(__dirname, 'public', 'input.html'));
-    });
+    //});
 });
 
 app.post('/send',(req, res) => {
+  var send_context=null;
+  var index=0;
+  if(req.cookies.id!=null){
+    for(index=0;index<context.length;index++){
+        if(context[index].conversation_id==req.cookies.id)
+          {send_context=context[index];break;}
+    }
+  }
     conversation.message({
       input: {'text': req.body.text},
       workspace_id: '1cf9cd65-5506-4bc2-a31d-06872dec90dd',
-      context: context
+      context: send_context
   },  function(err, response) {
       if (err)
       console.log('error:', err);
       else
      //console.log(JSON.stringify(response, null, 2));
-     context= response.context;
+    if(send_context==null)
+      context.push(response.context);
+    else
+     context[index]= response.context;
+     res.cookie('id', response.context.conversation_id);
      res.send(response.output.text);
     });
 });
 
 app.listen(8080, function () {
-  	console.log('App listening on port 8080!');
+    console.log('App listening on port 8080!');
 });
 
--- microservices/ui/app/src/App.js
+++ microservices/ui/app/src/App.js
@@ -46,7 +46,7 @@ Watson.defaultProps = {
 const steps = [
 {
   id: 'first',
-  message: 'Hi. It looks like a nice drive today. What would you like me to do?',
+  message: 'type somthing to start',
   trigger: 'thisOne'
 },
 {
